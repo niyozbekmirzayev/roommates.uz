0ccdcb591f94b84ff448a36b68b81e937b1f6e1c
Niyozbek <n.mirzayev@baik.uz>
2023-04-19 14:40:56 +0500

    Default Scheme changed

--- Roommates.Data/Migrations/20230419093637_DefaultSchemeChanged.Designer.cs
+++ Roommates.Data/Migrations/20230419093637_DefaultSchemeChanged.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Roommates.Data;
@@ -11,9 +12,10 @@ using Roommates.Data;
 namespace Roommates.Data.Migrations
 {
     [DbContext(typeof(RoommatesDbContext))]
-    partial class RoommatesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230419093637_DefaultSchemeChanged")]
+    partial class DefaultSchemeChanged
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
