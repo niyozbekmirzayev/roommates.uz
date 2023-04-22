70a08260aa33e77386390e1c0f349afaa32fb0e0
Niyozbek Mirzayev <n.mirzayev@baik.uz>
2023-04-22 11:40:17 +0500

    minor change

--- Roommates.Service/Response/ResponseCodes.cs
+++ Roommates.Service/Response/ResponseCodes.cs
@@ -5,7 +5,7 @@
         public const string SUCCESS_ADD_DATA = "success-add-data";
         public const string SUCCESS_GENERATE_TOKEN = "success-generate-token";
 
-        public const string UNEXPECTED_ERROR_EXCEPTION = "unexpected-error";
+        public const string UNEXPECTED_ERROR_EXCEPTION = "unexpected-error-exception";
         public const string ERROR_SAVE_DATA = "error-save-data";
         public const string ERROR_DUPLICATE_DATA = "error-duplicate-data";
         public const string ERROR_NOT_FOUND_DATA = "error-not-found-data";
